42307e3755608316eb81852fb3cb3719889c1311
ejYang <yejyang@kaist.ac.kr>
2017-05-03 14:56:59 +0900

    revise distmodel.py file -- layer level description

--- distmodel.py
+++ distmodel.py
@@ -1,66 +1,34 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
+
 import distutil as util
 import tensorflow as tf
+import layer
 
 
 
 def node1(images, FLAGS):
-  # conv1
-  with tf.variable_scope('conv1') as scope:
-    kernel = util._variable_with_weight_decay('weights', shape=[5, 5, 3, 64],
-                                         stddev=1e-4, wd=0.0)
-    conv = tf.nn.conv2d(images, kernel, [1, 1, 1, 1], padding='SAME')
-    biases = util._variable_on_cpu('biases', [64], tf.constant_initializer(0.0))
-    bias = tf.nn.bias_add(conv, biases)
-    conv1 = tf.nn.relu(bias, name=scope.name)
-    util.print_activations(conv1)
 
-  # pool1
-  pool1 = tf.nn.max_pool(conv1, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1],
-                         padding='SAME', name='pool1')
-  # norm1
-  norm1 = tf.nn.lrn(pool1, 4, bias=1.0, alpha=0.001 / 9.0, beta=0.75,
-                    name='norm1')
+    conv1 = layer._conv(images, 3, 64, 5, 5, 1, 1, 'SAME')
+    pool1 = layer._mpool(conv1, 3, 3, 2, 2)
+    norm1 = layer._norm(pool1, 4., 1.0, 0.001 / 9.0, 0.75)
+    conv2 = layer._conv(norm1, 64, 64, 5, 5, 1, 1, 'SAME')
+    norm2 = layer._norm(conv2, 4., 1.0, 0.001 / 9.0, 0.75)
+    pool2 = layer._mpool(norm2, 3, 3, 2, 2)
 
-  # conv2
-  with tf.variable_scope('conv2') as scope:
-    kernel = util._variable_with_weight_decay('weights', shape=[5, 5, 64, 64],
-                                         stddev=1e-4, wd=0.0)
-    conv = tf.nn.conv2d(norm1, kernel, [1, 1, 1, 1], padding='SAME')
-    biases = util._variable_on_cpu('biases', [64], tf.constant_initializer(0.1))
-    bias = tf.nn.bias_add(conv, biases)
-    conv2 = tf.nn.relu(bias, name=scope.name)
-    util.print_activations(conv2)
-  # norm2
-  norm2 = tf.nn.lrn(conv2, 4, bias=1.0, alpha=0.001 / 9.0, beta=0.75,
-                    name='norm2')
-  # pool2
-  pool2 = tf.nn.max_pool(norm2, ksize=[1, 3, 3, 1],
-                         strides=[1, 2, 2, 1], padding='SAME', name='pool2')
+    return pool2
 
-  return pool2
 
 def node2(pool2, FLAGS):
+
   # local3
-  with tf.variable_scope('local3') as scope:
-    # Move everything into depth so we can perform a single matrix multiply.
-    reshape = tf.reshape(pool2, [FLAGS.batch_size, -1])
-    dim = reshape.get_shape()[1].value
-    weights = util._variable_with_weight_decay('weights', shape=[dim, 384],
-                                          stddev=0.04, wd=0.004)
-    biases = util._variable_on_cpu('biases', [384], tf.constant_initializer(0.1))
-    local3 = tf.nn.relu(tf.matmul(reshape, weights) + biases, name=scope.name)
-    util.print_activations(local3)
+  reshape = tf.reshape(pool2, [FLAGS.batch_size, -1])
+  dim = reshape.get_shape()[1].value
+  local3 = layer._affine(reshape, dim, 384)
 
   # local4
-  with tf.variable_scope('local4') as scope:
-    weights = util._variable_with_weight_decay('weights', shape=[384, 192],
-                                          stddev=0.04, wd=0.004)
-    biases = util._variable_on_cpu('biases', [192], tf.constant_initializer(0.1))
-    local4 = tf.nn.relu(tf.matmul(local3, weights) + biases, name=scope.name)
-    util.print_activations(local4)
+  local4 = layer._affine(local3, 384, 192)
 
   # softmax, i.e. softmax(WX + b)
   with tf.variable_scope('softmax_linear') as scope:
